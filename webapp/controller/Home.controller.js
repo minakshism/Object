1b01b082e1527ccc7d18d103c43b2695c952013b
minakshism <minakshi129@gmail.com>
2018-03-11 07:36:34 +0000

    changes done on home and controller

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,11 +1,17 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 sap.ui.define([
+	"jquery.sap.global",
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function(jQuery, Controller) {
 	"use strict";
 
 	return Controller.extend("ca.controller.Home", {
-
+		onInit : function(){
+			this.oModel = new sap.ui.model.json.JSONModel();
+			this.oModel.loadData(jQuery.sap.getModulePath("ca.util", "/countries.json"));
+			this.getView().setModel(this.oModel);
+			
+		}
 	
 	});
 
